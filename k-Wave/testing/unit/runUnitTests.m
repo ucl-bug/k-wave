a7fab8c217be608ac9722a0e388319de6101e60a
Bradley Treeby <b.treeby@ucl.ac.uk>
2025-03-04 17:46:43 +0000

    Allow wildcard input for runUnitTests

--- k-Wave/testing/unit/runUnitTests.m
+++ k-Wave/testing/unit/runUnitTests.m
@@ -26,10 +26,10 @@ function runUnitTests(wildcard)
 % ABOUT:
 %     author        - Bradley Treeby
 %     date          - 17th February 2014
-%     last update   - 4th March 2024
+%     last update   - 4th March 2025
 %       
 % This function is part of the k-Wave Toolbox (http://www.k-wave.org)
-% Copyright (C) 2014-2024 Bradley Treeby
+% Copyright (C) 2014-2025 Bradley Treeby
 
 % This file is part of k-Wave. k-Wave is free software: you can
 % redistribute it and/or modify it under the terms of the GNU Lesser
