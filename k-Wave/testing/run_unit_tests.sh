8b01c25e682e6c5753e4f0fae9f869a5e35c2553
Stella <30465823+stellaprins@users.noreply.github.com>
2025-07-07 21:07:21 +0100

    Add libxml2 update step and logging MATLAB output

--- k-Wave/testing/run_unit_tests.sh
+++ k-Wave/testing/run_unit_tests.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ## Bash script to call runUnitTests for GitLab CI.
 cd "$(dirname "$0")/unit"
-matlab -desktop -nojvm -r "test_results_json = runUnitTests();"
+matlab -nojvm -r "test_results_json = runUnitTests();" -logfile matlab.log
 exit $?
