dc3a6f8357ae9a6bbe9620fabcbda63c1ccdc226
Stella <30465823+stellaprins@users.noreply.github.com>
2025-07-15 14:46:35 +0100

    fix script

--- k-Wave/edit_html_files.m
+++ k-Wave/edit_html_files.m
@@ -1,9 +1,6 @@
+% filepath: fix_matlab_links_in_helpfiles.m
 % Replace broken MATLAB editor/run HTML links with plain text instructions.
-%
-% To address issue #5
-% 
-% In short, MATLAB 2024b+ opens help files in the system browser, breaking direct links.
-% This script updates HTML files to show manual command instructions instead.
+% Only targets <ul>...</ul> blocks inside <p>...</p> that contain both edit and run links.
 
 rootDir = pwd; % or specify your root directory
 htmlFiles = dir(fullfile(rootDir, '**', '*.html'));
@@ -12,25 +9,34 @@ for k = 1:numel(htmlFiles)
     filePath = fullfile(htmlFiles(k).folder, htmlFiles(k).name);
     txt = fileread(filePath);
 
-    % Replace edit links
-    editPattern = '<li><a href="matlab:edit\(\[getkWavePath\(''([^'']*)''\) ''([^'']+?\.m)''\]\);" target="_top">Open the file in the MATLAB Editor</a></li>';
-    editReplace = sprintf(['To open the file in MATLAB, enter the following command in the MATLAB Command Window:\n\nedit(getkWavePath(''$1'') ''$2'');\n']);
+    % Pattern to match a <p> containing a <ul> with edit/run links (case-insensitive, multiline)
+    pattern = ['<p>\s*<ul>\s*' ...
+        '<li><a href="matlab:edit\(\[getkWavePath\(''([^'']*)''\) ''([^'']+?\.m)''\]\);" target="_top">Open the file in the MATLAB Editor</a></li>\s*' ...
+        '<li><a href="matlab:run\(\[getkWavePath\(''([^'']*)''\) ''([^'']+?)''\]\);" target="_top">Run the file in MATLAB</a></li>\s*' ...
+        '</ul>\s*</p>'];
 
-    txt = regexprep(txt, editPattern, editReplace, 'dotall');
+    % Replacement with <p> preserved, but <ul>, <li>, and <a> replaced with instructions and code blocks
+    replace = [newline ...
+        'To open the file in MATLAB, enter the following command in the MATLAB Command Window.' newline ...
+        '<pre class="codeinput">' newline ...
+        'edit(getkWavePath(''$1'') ''$2'');' newline ...
+        '</pre>' newline ...
+        'To run the file, enter the following command in the MATLAB Command Window.' newline ...
+        '<pre class="codeinput">' newline ...
+        'run(getkWavePath(''$3'') ''$4'');' newline ...
+        '</pre>' newline ...
+        '</p>'];
 
-    % Replace run links (with or without .m extension)
-    runPattern = '<li><a href="matlab:run\(\[getkWavePath\(''([^'']*)''\) ''([^'']+?)(\.m)?''\]\);" target="_top">Run the file in MATLAB</a></li>';
-    runReplace = sprintf(['To run the file, enter the following command in the MATLAB Command Window:\n\nrun(getkWavePath(''$1'') ''$2'');\n']);
-
-    txt = regexprep(txt, runPattern, runReplace, 'dotall');
+    % Do the replacement
+    newtxt = regexprep(txt, pattern, replace, 'dotall');
 
     % Only write if changes were made
-    if ~strcmp(fileread(filePath), txt)
+    if ~strcmp(txt, newtxt)
         fid = fopen(filePath, 'w');
         if fid == -1
             warning('Could not open file for writing: %s', filePath);
         else
-            fwrite(fid, txt);
+            fwrite(fid, newtxt);
             fclose(fid);
             fprintf('Updated: %s\n', filePath);
         end
